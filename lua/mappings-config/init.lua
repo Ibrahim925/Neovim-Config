b7bd6ba723dc0c07656d7c5eb95fa6e70890d802
Ibrahim Khawar <ikb1703@gmail.com>
2022-08-03 14:22:03 -0400

    Added completion

--- lua/mappings-config/init.lua
+++ lua/mappings-config/init.lua
@@ -1,14 +1,4 @@
 vim.cmd([[
-    function! s:check_back_space() abort
-    let col = col('.') - 1
-    return !col || getline('.')[col - 1]  =~ '\s'
-    endfunction
-
-    inoremap <silent><expr> <Tab>
-        \ pumvisible() ? "\<C-n>" :
-        \ <SID>check_back_space() ? "\<Tab>" :
-        \ coc#refresh()
-
     let mapleader = "\<Space>"
     inoremap jj <Esc>
 
--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -1,5 +1,6 @@
 return require("packer").startup(function(use)
     use "wbthomason/packer.nvim"
+
     --Appearance
     use "doums/darcula"
     use "junegunn/rainbow_parentheses.vim"
@@ -7,12 +8,25 @@ return require("packer").startup(function(use)
     use "lukas-reineke/indent-blankline.nvim"
     use "nvim-treesitter/nvim-treesitter"
     use "vim-airline/vim-airline"
-    --Text editing
+
+    --Text editing--
     use "windwp/nvim-autopairs"
+
+    -- Completion
+    use "hrsh7th/nvim-cmp"
+    use "hrsh7th/cmp-buffer"
+    use "hrsh7th/cmp-path"
+    use "hrsh7th/cmp-cmdline"
+    use "saadparwaiz1/cmp_luasnip"
+
+    use "L3MON4D3/LuaSnip"
+    use "rafamadriz/friendly-snippets"
+
     --File management
     use "kyazdani42/nvim-tree.lua"
     use "airblade/vim-rooter"
     use "akinsho/bufferline.nvim"
+
     --Work flow
     use "ctrlpvim/ctrlp.vim"
     use "tpope/vim-fugitive"
